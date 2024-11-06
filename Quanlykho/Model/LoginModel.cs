10c783923fcfb635bbd241ae12163678b17816e6
vancong <108638136+NguyenCong2806@users.noreply.github.com>
2024-11-06 12:06:27 +0700

    upcode

---
+++ Quanlykho/Model/LoginModel.cs
@@ -0,0 +1,28 @@
+﻿using Quanlykho.Utilities;
+
+namespace Quanlykho.Model
+{
+    public class LoginModel : ViewModelBase
+    {
+        private bool _isCheckLogin { get; set; }
+        public bool IsCheckLogin
+        {
+            get { return _isCheckLogin; }
+            set
+            {
+                _isCheckLogin = value;
+                OnPropertyChanged(nameof(IsCheckLogin));
+            }
+        }
+        private string _notification { get; set; }
+        public string Notification
+        {
+            get { return _notification; }
+            set
+            {
+                _notification = value;
+                OnPropertyChanged(nameof(Notification));
+            }
+        }
+    }
+}
